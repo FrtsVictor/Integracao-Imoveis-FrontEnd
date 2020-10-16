bd41cefd9995fc4b8b7f4b171e5d6c94ed200b83
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-16 19:18:15 -0300

    Creating  Card  /components

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CardContainer, DivImg } from './styles';
+
+const Cards = () => (
+  <CardContainer>
+    <DivImg />
+  </CardContainer>
+);
+
+export default Cards;
---
+++ src/components/Card/styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+    display: flex;
+    background-color: red;
+    width: 50vh;
+    height: 16vh;
+    justify-content: center;
+    align-content: stretch;
+`;
+
+export const Img = styled.image`
+
+`;
+
+export const DivImg = styled.div`
+display: flex;
+flex-direction: row;
+height: 50%;
+width: 50%;
+background-color: black;
+align-self: flex-start;
+`;
