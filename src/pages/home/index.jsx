f1f9526c600f9dc1e995e47a0eab26fa5c766a27
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-17 23:39:08 -0300

    AddCards & props validaiton  /pages Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 import Carousel from '../../components/Carousel';
 
-const Home = () => (
+import { Container } from './styles';
 
-  <>
-    <Header title="Home" />
-    <Carousel />
-    {/* <Card /> */}
-  </>
+const Home = () => {
+  // _________________Test
+  const titulo = 'Apartamento com 2 quartos sala e banheiro mais sala de estar';
+  const valor = '280.000.00';
+  const tipo = 'Apartamento - APT';
+  const bairro = 'Agrioes';
+  const cidade = 'Teresópolis';
+  const dormitorios = 1;
+  const banheiros = 1;
+  const transacao = 'Venda';
+  const destaque = true;
 
-);
+  return (
+    <>
+      <Header title="Home" />
+      <Carousel />
+      <Container>
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
 
+        <Card
+          titulo={titulo}
+          valor={valor}
+          bairro={bairro}
+          tipo={tipo}
+          cidade={cidade}
+          dormitorios={dormitorios}
+          banheiros={banheiros}
+          transacao={transacao}
+          destaque={destaque}
+        />
+
+      </Container>
+    </>
+  );
+};
 export default Home;
