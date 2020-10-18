efeb7aca9ab51ed5cbb031f25866ec8eb1fd5d20
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-17 23:37:10 -0300

    Add Card props & styles /components Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
-import { CardContainer, DivImg } from './styles';
 
-const Cards = () => (
+// _______________Icons
+import ApartmentIcon from '@material-ui/icons/Apartment';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import RoomIcon from '@material-ui/icons/Room';
+import BathtubIcon from '@material-ui/icons/Bathtub';
+import HotelIcon from '@material-ui/icons/Hotel';
+import ShuffleSharpIcon from '@material-ui/icons/ShuffleSharp';
+import FavoriteBorderSharpIcon from '@material-ui/icons/FavoriteBorderSharp';
+
+// _______________Styles
+import {
+  CardContainer, HouseImg, CardP, Info, LineDiv, IconDiv, FavIcon,
+} from './styles';
+
+const Card = ({
+  cidade, valor, bairro, tipo, banheiros, dormitorios, transacao,
+}) => (
+
   <CardContainer>
-    <DivImg />
+    <HouseImg />
+    <Info>
+      <FavIcon>
+        <FavoriteBorderSharpIcon />
+      </FavIcon>
+
+      <CardP>
+        <ApartmentIcon />
+        {tipo}
+      </CardP>
+
+      <CardP>
+        <RoomIcon />
+        {cidade}
+        {' / '}
+        {bairro}
+      </CardP>
+
+      <CardP>
+        <AttachMoneyIcon />
+        {valor}
+      </CardP>
+
+      <LineDiv>
+        <CardP>
+          <IconDiv>
+            <BathtubIcon />
+            {banheiros}
+          </IconDiv>
+        </CardP>
+
+        <CardP>
+          <IconDiv>
+            <HotelIcon />
+            {dormitorios}
+          </IconDiv>
+        </CardP>
+
+        <CardP>
+          <IconDiv>
+            <ShuffleSharpIcon />
+            {transacao}
+          </IconDiv>
+        </CardP>
+
+      </LineDiv>
+    </Info>
   </CardContainer>
 );
 
-export default Cards;
+export default Card;
--- src/components/Card/styles.jsx
+++ src/components/Card/styles.jsx
@@ -1,17 +1,25 @@
 import styled from 'styled-components';
+import Myimg from '../../assets/casa1.jpg';
 
 export const CardContainer = styled.div`
+    margin-top: 60px;
     display: flex;
-    background-color: red;
-    width: 50vh;
-    height: 16vh;
-    justify-content: center;
-    align-content: stretch;
+    width: 50vw;
+    height: 30vh;
+    padding: 5px;
+    background-color:#ebebed;
+
+    && ::hover{
+        cursor: pointer;
+    }
 `;
 
-export const Img = styled.image`
-
-`;
+export const HouseImg = styled.img.attrs((props) => ({
+  src: props.Img || Myimg,
+}))`
+    width: 50%;
+    height: 100%;
+  `;
 
 export const DivImg = styled.div`
 display: flex;
@@ -20,4 +28,45 @@ height: 50%;
 width: 50%;
 background-color: black;
 align-self: flex-start;
+`;
+
+export const CardP = styled.p`
+font-size: 0.8rem;
+margin-left: 15px;
+width:100%;
+display:flex;
+flex: 1;
+`;
+
+export const PContainer = styled.div`
+display:flex;
+background-color: red;
+border: 1px solid black;
+height: 20px;
+flex: 1;
+`;
+
+export const Info = styled.div`
+display:flex;
+flex-direction: column;
+flex: 1;
+`;
+
+export const LineDiv = styled.div`
+display: flex;
+width:100%;
+
+`;
+
+export const IconDiv = styled.div`
+display:flex;
+flex-direction:column;
+align-items: center;
+width: 100%;
+margin-bottom: 3vh;
+`;
+
+export const FavIcon = styled.div`
+display: flex;
+justify-content: flex-end;
 `;
