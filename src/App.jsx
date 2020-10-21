445802aefd67660a992ecbdac039eaa1368262fd
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-21 14:21:22 -0300

    Add Footer /components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-import Footer from './components/Footer';
+import Footer from './components/Footerrrrr';
 import GlobalStyle from './styles/global';
 
 const App = () => (
