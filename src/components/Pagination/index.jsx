1b4ee2270b8803acb0339025a8eb1c411a226c0c
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-22 12:58:28 -0300

    Add Pagination /components

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function PaginationControlled({ pageable, getPages }) {
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+
+  return (
+    <div className={classes.root}>
+      <Pagination
+        count={pageable.totalPages}
+        page={page}
+        onChange={(event, value) => {
+          setPage(value);
+          getPages(value - 1);
+        }}
+      />
+    </div>
+  );
+}
