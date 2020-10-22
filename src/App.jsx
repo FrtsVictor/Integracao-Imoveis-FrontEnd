eb569495e82e97a5d2c532f6f5f758e427b2dfa0
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-22 12:56:30 -0300

    Removed Global Footer App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-import Footer from './components/Footerrrrr';
 import GlobalStyle from './styles/global';
 
 const App = () => (
   <>
     <BrowserRouter>
       <Routes />
-      <Footer />
     </BrowserRouter>
     <GlobalStyle />
   </>
