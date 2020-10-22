76ab9aabfc46cf07cfa2aaec148622919e5d3453
Daniel Araújo <danielcouto.rj@gmail.com>
2020-10-22 01:06:07 -0300

    Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Container, Icones, LogoImg, LogoContainer, TextContainer,
+} from './styles';
 
 const Footer = () => (
 
   <Container>
-    <h2>Integração Imóveis</h2>
-    <p>Produzido por Residentes Serratec 2020</p>
+    <LogoContainer>
+      <LogoImg />
+    </LogoContainer>
+
+    <TextContainer>
+      <p>
+        <b><em>Produzido por Residentes Serratec 2020 \o/</em></b>
+      </p>
+    </TextContainer>
+
+    <Icones>
+      <a href="https://instagram.com" className="fa fa-instagram" target="_blank" />
+      <a href="https//facebook.com" className="fa fa-facebook" target="_blank" />
+      <a href="https://twitter.com" className="fa fa-twitter" target="_blank" />
+    </Icones>
   </Container>
 
 );
