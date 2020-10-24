7562fb6a809fbd64dbbe66508a49cc67fb3da67f
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-23 22:48:10 -0300

    Add private routes  /routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Cadastro from '../pages/cadastro';
 import Favoritos from '../pages/favoritos';
 import Home from '../pages/home';
 import Login from '../pages/login';
 import Search from '../pages/search';
+import Route from './authRoute';
 
 const Routes = () => (
   <Switch>
     <Route path="/" component={Home} exact />
     <Route path="/Home" component={Home} />
-    <Route path="/search" component={Search} />
+    <Route path="/search" component={Search} isPrivate />
     <Route path="/Login" component={Login} />
     <Route path="/Cadastro" component={Cadastro} />
-    <Route path="/Favoritos" component={Favoritos} />
+    <Route path="/Favoritos" component={Favoritos} isPrivate />
   </Switch>
 );
 
