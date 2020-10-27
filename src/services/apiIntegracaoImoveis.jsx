c3851d1168964de6eae294ee77e31bcf49a41e90
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-27 20:25:01 -0300

    Add add and remove imovel methods /services apiIntegracaoImoveis

--- src/services/apiIntegracaoImoveis.jsx
+++ src/services/apiIntegracaoImoveis.jsx
@@ -6,8 +6,7 @@ const api = axios.create({
 });
 
 const ApiImoveis = {
-
-  get: async (page = 0) => {
+  getAll: async (page = 0) => {
     try {
       const response = await api.get(`api/imoveis?page=${page}`);
       return response.data;
@@ -31,6 +30,14 @@ const ApiImoveis = {
       return console.log(error.response.status);
     }
   },
+  getByUserId: async (userId) => {
+    try {
+      const response = await api.get(`api/imoveis/users/${userId}`);
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
 
   user: {
     login: async (username, password) => {
@@ -39,27 +46,43 @@ const ApiImoveis = {
           username, password,
         });
         console.log('login', response);
+        swal('LoggedIn!', 'Bem vindo ao nosso sistema!', 'sucess');
         return response;
       } catch (error) {
-        return console.log(error);
+        if (error.response.status === 401) { swal('Ops!', 'username ou senha invalidos', 'error'); }
+        console.log(error);
+        return null;
       }
     },
+
     getByUserName: async (username, token) => {
       try {
         const response = await api.get(`/api/username/${username}`);
         console.log('login', response);
         return { ...response.data, token };
       } catch (error) {
+        if (error.response.status === 404) { swal('Ops!', 'username invalido', 'error'); }
         return console.log(error);
       }
     },
-    // addImovel: async () => {
-    //   try {
 
-    //   } catch (error) {
+    addImovel: async (userId) => {
+      try {
+        const response = await api.post(`/api/imoveis/users/${userId}`);
+        return response;
+      } catch (error) {
+        return console.log(error);
+      }
+    },
 
-    //   }
-    // },
+    removeImovel: async (imovelId) => {
+      try {
+        const response = await api.put(`/api/imoveis/user/${imovelId}`);
+        return response;
+      } catch (error) {
+        return console.log(error);
+      }
+    },
   },
 };
 
