72863fdde7e414b45a04106887cf8fa4387730bd
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-29 15:33:01 -0300

    Add GlobalContants  /src

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
 export const LOCAL_STORAGE_KEYS = {
-  userAuthRoken: '@Integracao:userAuthRoken',
+  userAuthToken: '@Integracao:userAuthToken',
+  userImobileToken: '@Imobile:userAuthToken',
+};
+
+export const LOCAL_USER_IMOBILE = {
+  idAdministradora: '4f835cba-7c9c-4b99-a4f3-03fcb2b6142d',
+  login: 'paulo-cesar',
+  senha: '798788',
 };
