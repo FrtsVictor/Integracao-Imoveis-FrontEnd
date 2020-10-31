a971b924da07dc6d7d39fa2473262aa5736bc0cd
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-31 10:45:03 -0300

    Add Backgorund image  and styles /pages login

--- src/pages/login/styles.jsx
+++ src/pages/login/styles.jsx
@@ -1,22 +1,45 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import BackGroundImg from '../../assets/background_signUp.jpg';
 
-// export const Form = styled.div`
-//     width: 50%;
-//     margin-left: auto;
-//     margin-right: auto;
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-//     input {
-//         width: 100%;
-//         margin-bottom: 20px;
-//     }
+export const Background = styled.div`
+display: flex;
+height:100%;
+width:100%;
+max-width:100%;
+max-height:100%;
+background-image: url(${BackGroundImg});
+`;
 
-//     h3 {
-//         margin-bottom: 30px;
-//     }
+export const LogoContainer = styled.div`
+align-items:center;
+display:flex;
+flex:1;
+justify-content:center;
+mar
 
-// `;
-
-// export const Address = styled.div`
-
-
-// `;
+img{
+        min-width: 250px;
+        width: 5vw;
+    }
+`;
