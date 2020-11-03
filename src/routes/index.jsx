4d42a6eeb8f38b4e641986dd491ad2eb76fdf23b
FrtsVictor <victorfreitas.job@gmail.com>
2020-11-03 09:56:03 -0300

    Fixing Imports and Removing export default

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import Cadastro from '../pages/cadastro';
-import Favoritos from '../pages/favoritos';
-import Home from '../pages/home';
-import Login from '../pages/login';
-import Search from '../pages/search';
+import { Cadastro } from '../pages/cadastro';
+import { Favoritos } from '../pages/favoritos';
+import { Home } from '../pages/home';
+import { Login } from '../pages/login';
+import { Search } from '../pages/search';
 import Route from './authRoute';
 
-const Routes = () => (
+export const Routes = () => (
   <Switch>
     <Route path="/" component={Home} exact />
     <Route path="/Home" component={Home} />
     <Route path="/Login" component={Login} />
     <Route path="/Cadastro" component={Cadastro} />
     <Route path="/search" component={Search} />
-    <Route path="/Favoritos" component={Favoritos}  />
+    <Route path="/Favoritos" component={Favoritos} isPrivate />
   </Switch>
 );
-
-export default Routes;
