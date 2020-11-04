11278f43badce9ae3a142850bc0d3fd738839065
FrtsVictor <victorfreitas.job@gmail.com>
2020-11-03 23:58:09 -0300

    Refactor styles /pages search

--- src/pages/search/styles.jsx
+++ src/pages/search/styles.jsx
@@ -3,44 +3,60 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     font-family: 'Oxygen', sans-serif;
-    height:450px;
-    margin-bottom:70%;
-
+    min-height:100%;
+    min-width: 100%;
+    height:100%;
+    width:100%;
 `;
+
+export const DivPagination = styled.div`
+    display: flex;
+    align-items: center;
+ `;
+
 export const Filter = styled.div`
     display: flex;
+    flex-wrap:wrap;
     flex-direction: column;
     width: 20%;
+    max-width: 300px;
     border-right: 1px solid #ccc;
-    margin: 10px;
+    margin-right: 10px;
     background-color:#0071C2 ;
-    margin-top: -45px;
-    align-items: center;
-    margin-top:-20px;
+    opacity: 0.8;
+    align-items: flex-start;
+    height:80%;
 
     h2{
+        text-align:center;
         margin: 25px 5px;
         font-size:1.9rem;
-
     }
+
     h3{
+        text-align:center;
         margin: 25px 5px;
         font-size:1.2rem;
-
-    }
-
-       label{
-        margin-left:13px;
-        font-size:20px;
-
-
     }
 `;
+
 export const InputType = styled.div`
-    display: inline;
+    display: flex;
+    justify-content: center;
     flex-direction: column;
-    margin-right:80px;
+    align-items: flex-start;
+    margin-left:10px;
     margin-bottom:50px;
+
+    div{
+        display: flex;
+        flex-wrap: wrap;
+
+        label{
+            margin-left:13px;
+            font-size:20px;
+        }
+    }
 
 
 `;
@@ -56,53 +72,44 @@ export const Price = styled.div`
         font-weight:bold;
         width: 30%;
         cursor: pointer;
-
     }
 `;
+
 export const PriceActive = styled.div`
     display: flex;
     cursor: pointer;
-
 
     div {
         margin-left: auto;
         margin-right: 5px;
         display: flex;
-        :hover{
+
+        :hover {
             cursor: pointer;
         }
-
 
         span{
             margin-top: 2px;
             font-size: 12px;
             margin-left:17px;
             font-weight:bold;
-
-
         }
-
     }
 
 `;
 
 export const CardSection = styled.div`
-    display: grid;
-    justify-content: space-between;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 1%;
-    width: 80%;
-    height:50%;
-    margin-top:-50px;
-
-    a {
-        text-decoration: none;
-        color: black;
-
-    }
-
-    span {
-        margin-top: 30px;
-    }
+    display: flex;
+    align-items:center;
+    flex-wrap: wrap;
+    /* justify-content:space-evenly; */
+    gap: 10px;
+    max-width: 70%;
+    height: 100%;
 `;
 
+export const Paginations = styled.div`
+display: flex;
+align-items:center;
+justify-content:center;
+margin:0;`;
