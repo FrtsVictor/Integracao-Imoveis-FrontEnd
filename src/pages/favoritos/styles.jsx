cf8bac8957d321794cb0e3a2dae1195493193cc6
FrtsVictor <victorfreitas.job@gmail.com>
2020-11-06 18:06:05 -0300

    Add background color Avatar /pages favoritos

--- src/pages/favoritos/styles.jsx
+++ src/pages/favoritos/styles.jsx
@@ -3,36 +3,40 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex:1;
-    `;
+    min-width: 100%;
+    min-height: 100%;
+`;
 
 export const Filter = styled.div`
     display: flex;
+    margin-top: 20px;
     width: 20%;
-    border-right: 1px solid #000;
-    min-height:100%;
-    background-color:grey;
- `;
+    min-height: 100%;
+    background-color: #0585e0;
+    padding:15px;
+`;
 
 export const CardContainer = styled.div`
-display:flex;
-flex-wrap:wrap;
-align-items: center;
-justify-content:center;
-height:100%;
+    display:flex;
+    flex-wrap:wrap;
+    align-items: center;
+    justify-content:center;
+    gap:5px 5px 0 5px;
+    height:100%;
 `;
 
 export const CardDiv = styled.div`
-display: flex;
-width: 30%;
-height: 70%;
-padding: 1% 0;
-min-width: 300px;
+    display: flex;
+    width: 30%;
+    height: 70%;
+    padding: 1% 0;
+    min-width: 300px;
 `;
 
 export const PageDiv = styled.div`
-display: flex;
-align-items:center;
-justify-content:center;
+    display: flex;
+    align-items:center;
+    justify-content:center;
 `;
 
 export const InputType = styled.div`
@@ -41,17 +45,17 @@ export const InputType = styled.div`
 `;
 
 export const SearchTitle = styled.div`
-display: flex;
-flex-direction:column;
+    display: flex;
+    flex-direction:column;
 `;
 
 export const LineInput = styled.div`
-display: flex;
+    display: flex;
 `;
 
 export const FilterGrid = styled.div`
-display: grid;
-grid-template-columns: 1fr;
+    display: grid;
+    grid-template-columns: 1fr;
 
 label{
     display:inline-block;
