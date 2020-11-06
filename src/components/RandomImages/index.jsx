a556e4dcf13d35f2f91b6ae7dda08512c80c5b54
FrtsVictor <victorfreitas.job@gmail.com>
2020-11-06 18:07:19 -0300

    Fix RandomImage /components

--- src/components/RandomImages/index.jsx
+++ src/components/RandomImages/index.jsx
@@ -21,6 +21,6 @@ const houseImages = [
 ];
 
 export const RandomImg = () => {
-  const rand = Math.floor(Math.random() * 10);
+  const rand = Math.floor(Math.random() * 9);
   return houseImages[rand];
 };
