f8cadefe02ad63418755e4891ba4794cb422bf69
FrtsVictor <victorfreitas.job@gmail.com>
2020-11-05 23:44:37 -0300

    Fix pagination & add apiintegracaoImoveis Route /pages search

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,49 +1,19 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FiX } from 'react-icons/fi';
-import Icon from '@material-ui/core/Icon';
+import Pagination from '@material-ui/lab/Pagination';
 import { Card } from '../../components/Card';
-import api from '../../services/api';
 import { Footer } from '../../components/Footer';
 import {
-  Container, Filter, Price, PriceActive, CardSection, InputType, Paginations,
+  Container, Filter, Price, useStyles, PriceActive, CardSection, InputType, DivPagination,
 } from './styles';
+
 import { Header } from '../../components/NewHeader';
-
-import { PaginationHome } from '../../components/PaginationHome';
 import { apiImobile } from '../../services/apiImobile';
-
-// _______________________________
-import { Logo } from '../../components/NewHeader/Logo';
-import LogoImg from '../../assets/logo_1.png';
 
 // ________________________________________________________________________________
 export const Search = () => {
-//   const acessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZExvZ2luIjoiMzlmODExOWItYzA1OC04NTNjLTM3ZjMtYmUxYjE0NTMzZjA3IiwiaWRBZG1pbmlzdHJhZG9yYSI6IjRmODM1Y2JhLTdjOWMtNGI5OS1hNGYzLTAzZmNiMmI2MTQyZCIsImlkUGVzc29hIjoiMzlmODExOWItYmZjNC0yMDBlLTY3MzItOWUyMzVjZGI0M2Q0Iiwibm9tZSI6IlBhdWxvIENlc2FyIiwiZW1haWwiOiJkZW5pc2UuZHNuLmltYkBhbHRlcmRhdGEuY29tLmJyIiwicGVyZmlsIjoiQ29ycmV0b3IiLCJ1cmxJbWFnZW0iOiJodHRwczovL3dlYi1pbW1vYmlsZXdlYi5zMy5hbWF6b25hd3MuY29tL2NvcnJldGFnZW0vcHVibGljYS80ZjgzNWNiYS03YzljLTRiOTktYTRmMy0wM2ZjYjJiNjE0MmQvY29ycmV0b3I2MzczNzU5OTE2Njc2MzMyNzAucG5nIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDEyLyJ9.31Px5-gjnIzQDaEHsAmxs71rXmeijg75fOgk9jzd9UQ';
-//   const loadList = useCallback(
-//     async () => {
-//       try {
-//         const resp = await api.post('Imovel/obter-lista', {}, config);
-//         const test = resp.data.content.listaPaginada;
-//         setApiList(test);
-//         console.log(test);
-//         console.log('LoadList', apiList);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }, [],
-//   );
-
-  //   // _____________________________________________________________
-
-  //   useEffect(() => {
-  //     loadList();
-  //   }, []);
-
-  //   const config = {
-  //     headers: { Authorization: `bearer ${acessToken}` },
-  //   };
-
+  const classes = useStyles();
   const [apiList, setApiList] = useState([]);
   const history = useHistory();
   const [filteredproperty, setFilteredProperty] = useState(['']);
@@ -53,38 +23,28 @@ export const Search = () => {
   const [minValue, setMinValue] = useState();
   const [maxValue, setMaxValue] = useState();
   const [searchQuery, setSearchQuery] = useState([]);
-
+  // Paginations
+  const [totalPages, setTotalPages] = useState();
+  const [atualPage, setAtualPage] = useState(1);
+  const itensPorPagina = 6;
   const [pageable, setPageable] = useState({
     paginacao: {
-      itensPorPagina: 6,
-      paginaAtual: 0,
-      totalPaginas: 0,
+      paginaAtual: 1,
+      itensPorPagina,
     },
   });
-
-  //   const [itemList, setItemList] = useState([]);
-  const [atualPage, setAtualPage] = useState(1);
-
-  const getPages = (pages) => setAtualPage(pages);
 
   const getPaginationRequest = useCallback(() => {
     apiImobile.getImoveis(pageable)
       .then(({ data: { content } }) => {
-        const pagination = {
-          paginacao: {
-            itensPorPagina: 6,
-            paginaAtual: atualPage,
-            totalPaginas: Math.ceil(content.totalItens / 6) || 0,
-          },
-        };
-        setPageable(pagination);
+        setTotalPages(Math.ceil(content.totalItens / itensPorPagina));
         setApiList(content.listaPaginada);
       });
-  }, [pageable, atualPage]);
+  }, [pageable]);
 
   useEffect(() => {
     getPaginationRequest();
-  }, [atualPage]);
+  }, [pageable]);
 
   const loadFiltered = useCallback(
     () => {
@@ -145,13 +105,7 @@ export const Search = () => {
   return (
     <>
       <Header />
-
-      <Paginations>
-        <PaginationHome pageable={pageable} getPages={getPages} />
-      </Paginations>
-
       <Container>
-
         <Filter>
           <h2>Filtros</h2>
           <h3>Tipo</h3>
@@ -269,121 +223,26 @@ export const Search = () => {
         {/* </div> */}
 
       </Container>
+      <DivPagination>
+        <div className={classes.root}>
+          <Pagination
+            count={totalPages}
+            defaultPage={1}
+            siblingCount={3}
+            onChange={(e, value) => {
+              const pagination = {
+                paginacao: {
+                  itensPorPagina,
+                  paginaAtual: value,
+                },
+              };
+              setPageable(pagination);
+            }}
+          />
+        </div>
+      </DivPagination>
+
       <Footer styles={{ }} />
     </>
 
   );
-
-  //   return (
-  //     <>
-  //       <Header />
-  //       <Container>
-  //         <Filter>
-  //           <h2>Pesquisar</h2>
-
-  //           <h3>Preço</h3>
-  //           {!activePriceFilter
-  //             ? (
-  //               <Price>
-
-  //                 <input
-  //                   type="number"
-  //                   value={minValue}
-  //                   min="0"
-  //                   max={maxValue}
-  //                   onChange={(e) => setMinValue(e.target.value)}
-  //                   placeholder="R$ 0.00"
-  //                 />
-  //                 <p>até</p>
-  //                 <input
-  //                   type="number"
-  //                   value={maxValue}
-  //                   min={minValue || 0}
-  //                   onChange={(e) => setMaxValue(e.target.value)}
-  //                   placeholder="R$ 0.00"
-  //                 />
-  //                 <input
-  //                   type="button"
-  //                   value="Aplicar"
-  //                   onClick={() => minValue && maxValue && setActivePriceFilter(true)}
-  //                 />
-  //               </Price>
-  //             )
-  //             : (
-  //               <PriceActive>
-  //                 de
-  //                 {' '}
-  //                 {convertPrice(minValue)}
-  //                 {' '}
-  //                 até
-  //                 {' '}
-  //                 {convertPrice(maxValue)}
-  //                 <div onClick={() => {
-  //                   setMinValue();
-  //                   setMaxValue();
-  //                   setActivePriceFilter(false);
-  //                 }}
-  //                 >
-  //                   <span>Limpar</span>
-  //                 </div>
-
-  //               </PriceActive>
-  //             )}
-  //           <br />
-  //           <br />
-  //           <h3>Filtros</h3>
-
-  //           <InputType>
-  //             <input
-  //               type="checkbox"
-  //               onClick={(e) => addFilter(e.target)}
-  //               value="L"
-  //             />
-  //             <label htmlFor="">Locação</label>
-  //             <br />
-
-  //             <input
-  //               type="checkbox"
-  //               value="V"
-  //               onClick={(e) => addFilter(e.target)}
-  //             />
-  //             <label htmlFor="">Venda</label>
-  //             <br />
-
-  //             <input
-  //               type="checkbox"
-  //               value="Casa"
-  //               onClick={(e) => addFilterType(e.target)}
-  //             />
-  //             <label htmlFor="">Casa</label>
-  //             <br />
-
-  //             <input
-  //               type="checkbox"
-  //               value="Apartamento APT"
-  //               onClick={(e) => addFilterType(e.target)}
-  //             />
-  //             <label htmlFor="">Apartamento</label>
-  //           </InputType>
-
-  //         </Filter>
-
-  //         <CardSection>
-
-  //           {filteredproperty.map((imovel) => (
-  //             <div>
-  //               <Card
-  //                 imovel={{ ...imovel }}
-  //                 urlImagem={imovel.urlImagem}
-  //               />
-  //             </div>
-  //           ))}
-
-  //         </CardSection>
-
-  //       </Container>
-  //       <Footer />
-  //     </>
-
-//   );
-};
